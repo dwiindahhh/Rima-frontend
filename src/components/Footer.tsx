88660939014185a4117fd478087f0b466fb846f8
dwiindahhh <cahyaniindah146@gmail.com>
2025-09-22 16:20:58 +0700

    feat(footer): create footer web

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Logo from "../assets/rima_logo.png"; // sesuaikan path logo
+import Logo from "../assets/rima_logo.png"; 
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white shadow-inner">
-      <div className="grid grid-cols-3 items-center px-6 py-4">
+      <div className="grid grid-cols-3 items-center px-10 py-4">
         <div className="flex items-center space-x-2">
           <img src={Logo} alt="Rima Logo" className="h-8 w-8" />
           <span className="text-lg font-serif">RiMa</span>
