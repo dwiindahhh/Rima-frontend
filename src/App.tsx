5841738faad59d08cf0fb8fb5f3dbc48302ae9f6
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2025-09-23 22:12:19 +0700

    style(landing-page-section): create feature section base

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Features from "./components/sections/feature-section";
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-white">
       <Navbar />
       <Home />
-      
+      <Features />
       <Footer/>
     </div>
   );
